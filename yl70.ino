1879daba56c3df54621e1e06046d059ba67791fa
mgmoldova <platonvadim@ya.ru>
2019-05-02 13:13:51 +0300

    Added distance measuring

--- yl70.ino
+++ yl70.ino
@@ -1,8 +1,16 @@
-//PIN MAP
-#define ENCODER_1 50
-#define ENCODER_2 52
-#define OFST 0
+    
+/*
+коричневый +5 розовый
+белый с черным 0 черный
+синий ен1 красный
+белый с синим ен2 желтый
+*/
 
+//PIN MAP
+#define ENCODER_1 2
+#define ENCODER_2 3
+#define OFST 0
+#define ANTIFREEZE_DELAY 2000
 //#define PLOT 1
 int endcoder_timer = 20;
 int enc[2];
@@ -13,13 +21,20 @@ unsigned long Black_currentTime = millis();
 
 unsigned long White_rotation = millis();
 unsigned long White_currentTime = millis();
+unsigned long anti_freeze = millis();
+
+double total_distance = 0;
+uint16_t simple_distance = 0;
+
+bool warning = false;
 void setup()
 {
  Serial.begin(9600);
+ Serial.println("Encoder started..");
  pinMode(ENCODER_1, INPUT);
  pinMode(ENCODER_2, INPUT);
-
 }
+
 void loop()
 {
  enc[0] = digitalRead(ENCODER_1);
@@ -51,26 +66,55 @@ void loop()
 #endif
 //calculate rotation
 Black_currentTime = millis();
+anti_freeze = millis();
+warning = false;
 while(enc[0] == 1){
-  //start timer  
   enc[0] = digitalRead(ENCODER_1);
+  //anti-infinite loop
+  if(millis() - anti_freeze > ANTIFREEZE_DELAY)
+  {
+    warning = true;
+    break;
+  }
 }
 Black_rotation = millis() - Black_currentTime;
 
 White_currentTime = millis();
+anti_freeze = millis();
+
 while(enc[0] == 0){
-  //start timer  
   enc[0] = digitalRead(ENCODER_1);
+  //anti-infinite loop
+  if(millis() - anti_freeze > ANTIFREEZE_DELAY)
+  {
+    warning = true;
+    break;
+  }
 }
 White_rotation = millis() - White_currentTime;
+
+
 time_rotation = White_rotation + Black_rotation;
-if(time_rotation > 0){
+float speed_km_ora = 19.5/time_rotation*36;
+float speed_m_sec = speed_km_ora/3.6;
+
+if(warning)
+{
+  time_rotation = 0;
+  speed_km_ora = 0;
+  speed_m_sec = 0;
+}
+#ifndef PLOT
+if(time_rotation >= 0 && speed_km_ora <= 8.0){
+total_distance += speed_m_sec * time_rotation / 1000;
 Serial.print("Time: ");
-Serial.print(time_rotation);
-Serial.print("ms ");
-Serial.print(19.5/time_rotation*36);
-Serial.println("km/ora");
-}
+Serial.print(time_rotation); Serial.print("ms ");
+Serial.print(speed_km_ora); Serial.print("km/h | ");
+Serial.print(speed_m_sec); Serial.print("m/s ");
+Serial.print("Distance: ");
+Serial.print(total_distance); Serial.print("m || ");
+Serial.print(simple_distance); Serial.println("cm");
 
 }
-
+#endif
+}
